66ccd2450c4fc0ddc6368430dd4917a0f3726a21
Nolan Urban <nolanurban@gmail.com>
2022-07-18 10:00:06 -0500

    Added Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+// Utilize multiple variables and println
+
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson"; // string variable
+    int age = 47; // integer variable
+    String state = "Arkansas"; // also strings
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
