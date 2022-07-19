1f353228e1b2ad45fb7c1360ddf6dba3aaf857b8
Nolan Urban <nolanurban@gmail.com>
2022-07-19 13:19:43 -0500

    Had some fun with never ending for loops that crashed my shell.

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,7 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i;
+    for(i = 1 ; i <= 10; i++) System.out.println(i);
+    for(i--; i >= 1; i--) System.out.println(i);
+  }
+}
