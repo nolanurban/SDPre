e5adf7504f73a5ddafbe4394578894124e0bed91
Nolan Urban <nolanurban@gmail.com>
2022-07-19 12:56:54 -0500

    Finished Objective7Lab4 -- fairly easy

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum = 0; // our summation compoojnent
+    int counter = 1;
+
+    while (counter <= 20) {
+      sum += counter;
+      counter++;
+    }
+    System.out.println(sum);
+  }
+}
