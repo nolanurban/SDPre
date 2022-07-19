8cae776e0a3316f7f5c9fa0a4ce83f229e6eddce
Nolan Urban <nolanurban@gmail.com>
2022-07-19 12:23:19 -0500

    Completed while loop lab 1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,10 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10; // this we'll keep decrementing until it's 0;
+
+    while (counter > 0) {
+      System.out.println(counter);
+      counter--;
+    }
+  }
+}
