fb9f5204c92efbc6ef7f86e8743c8bb3ef2e7649
Nolan Urban <nolanurban@gmail.com>
2022-07-19 14:38:54 -0500

    Learned that void can be changed to the data type you wish to return, so that's neat.  I also felt my method was fairly clean.

---
+++ Labs/Objective9Lab4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Objective9Lab4 {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+		double num1, num2;
+
+    System.out.print("Please give me a number: ");
+		num1 = kb.nextDouble();
+		System.out.print("Please give me another number: ");
+		num2 = kb.nextDouble();
+
+    double average = findAverage(num1, num2);
+    System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
+
+    kb.close();
+
+  }
+  public static double findAverage(double var1, double var2) {
+    return ((var1 + var2) / 2);
+  }
+}
